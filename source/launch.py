0f75938fd6baf2d5c4e9999e5caea8cf0b9fa120
esther-obot <122905926+esther-obot@users.noreply.github.com>
2023-09-13 18:11:02 -0600

    Add files via upload

---
+++ source/launch.py
@@ -0,0 +1,63 @@
+"""Functions for GOPH 419 Lab Assignment #1."""
+
+import numpy as np
+
+
+def arcsin(x):
+    """Compute the inverse sine of x on the range [-pi/2, pi/2].
+    """
+    # add your code here
+    return ...
+
+
+def launch_angle(ve_v0, alpha):
+    """Calculate the launch angle from vertical given
+    velocity ratio and target altitude ratio.
+    """
+    #add your code here
+
+    return ...
+
+
+def launch_angle_range(ve_v0, alpha, tol_alpha):
+    """Calculate the range of launch angles for a given
+    velocity ratio, target altitude ratio, and tolerance.
+    """
+    #add your code here
+
+    return ...
+
+
+def min_altitude_ratio(ve_v0):
+    """Utility function for computing minimum possible peak altitude ratio
+    for a given velocity ratio.
+    """
+    #add your code here
+    return ...
+
+
+def max_altitude_ratio(ve_v0):
+    """Utility function for computing maximum possible peak altitude ratio
+    for a given velocity ratio.
+    """
+    #add your code here
+
+    return ...
+
+
+def min_velocity_ratio(alpha):
+    """Utility function for computing minimum possible velocity ratio
+    for a given target peak altitude ratio.
+    """
+    #add your code here
+
+    return ...
+
+
+def max_velocity_ratio(alpha):
+    """Utility function for computing maximum possible velocity ratio
+    for a given target peak altitude ratio.
+    """
+    #add your code here
+
+    return ...
