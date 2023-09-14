e29828c1cbcfec46a72e0b9433152781109c8f21
esther-obot <122905926+esther-obot@users.noreply.github.com>
2023-09-13 18:11:41 -0600

    Add files via upload

---
+++ source/driver.py
@@ -0,0 +1,33 @@
+"""Driver script for Assignment #1.
+
+See Also
+--------
+- Assignment description for mathematical and physics background.
+- launch.py for documentation of variables and functions.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from launch import (
+        min_altitude_ratio,
+        max_altitude_ratio,
+        min_velocity_ratio,
+        max_velocity_ratio,
+        launch_angle_range,
+)
+
+
+def main():
+    """Plot launch angle ranges given
+    values for ve_v0, alpha, and tol_alpha.
+    """
+
+    # plot launch angle range for a fixed velocity ratio
+    
+
+    # plot launch angle range for a fixed target altitude
+    
+
+if __name__ == "__main__":
+    main()
