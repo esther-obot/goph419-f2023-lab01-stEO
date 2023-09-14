2fe2abe179b320a725fa97de5c3ff66231ca6eb6
esther-obot <122905926+esther-obot@users.noreply.github.com>
2023-09-13 18:10:34 -0600

    Add files via upload

---
+++ source/tests.py
@@ -0,0 +1,10 @@
+"""Tests for GOPH 419 Lab Assignment #1."""
+
+import numpy as np
+
+from launch import (
+        arcsin,
+        launch_angle,
+        launch_angle_range,
+)
+# The test part depends on you.
